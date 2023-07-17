09f128e785d1c6651668fa2362d5b123d87de47e
Hsincho <99251348+xinzhuohkust@users.noreply.github.com>
2023-07-18 07:58:16 +0800

    Update shanghai_petition.R

--- shanghai_petition.R
+++ shanghai_petition.R
@@ -42,3 +42,37 @@ export(
       file = sprintf("data/table/%s_table.csv", Sys.Date()),
       bom = TRUE
     ) 
+
+extract_contents <- possibly(
+    \(link) {
+
+        contents <- link %>%
+            request() %>%
+            req_timeout(12) %>%
+            req_retry(
+                max_tries = 5,
+                max_seconds = 60,
+                backoff = ~6
+            ) %>%
+            req_perform() %>%
+            .$body %>%
+            read_html() %>%
+            html_text()
+        
+        Sys.sleep(sample(1:2, 1))
+        
+        return(contents)
+    },
+    otherwise = "error!"
+)
+
+contents <- table %>% 
+    transmute(links = sprintf(fmt = "https://zfwzzc.www.gov.cn/check_web/errorInfo_getErrorInfoList2.action?id=%s", id)) %>% 
+    distinct(links, .keep_all = TRUE) %>% 
+    mutate(contents = map_chr(links, extract_contents, .progress = TRUE))
+
+export(
+      contents, 
+      file = sprintf("data/contents/%s_table.csv", Sys.Date()),
+      bom = TRUE
+    ) 
