1470b17e8d3b9c13c669a508245c512fd7fd2d91
Hsincho <99251348+xinzhuohkust@users.noreply.github.com>
2023-07-18 07:30:41 +0800

    Update shanghai_petition.R

--- shanghai_petition.R
+++ shanghai_petition.R
@@ -2,3 +2,42 @@ library(tidyverse)
 library(jsonlite)
 library(rvest)
 library(httr2)
+
+extract_info <- possibly(
+    \(page) {
+        link <- str_c("https://zfwzzc.www.gov.cn/check_web/errorInfo_querySearch.action?term=&searchType=-1&pageNo=", page, "&pos=3&size=10&problemIdbg=-1")
+
+        info <- link %>%
+            request() %>%
+            req_timeout(12) %>%
+            req_retry(
+                max_tries = 5,
+                max_seconds = 60,
+                backoff = ~6
+            ) %>%
+            req_perform() %>%
+            .$body %>%
+            read_html() %>%
+            html_text()
+        
+        Sys.sleep(sample(1:2, 1))
+        
+        return(info)
+    },
+    otherwise = "error!"
+)
+
+total <- extract_info(1) %>% 
+    fromJSON() %>% 
+    pluck("iTotalDisplayRecords")
+
+info <- map(1:ceiling(total / 10), extract_info, .progress = TRUE)
+
+table <- info[info != "error!"] %>% 
+    map_dfr(\(x) fromJSON(x) |> pluck("body"))
+
+export(
+      table, 
+      file = sprintf("data/table/%s_table.csv", Sys.Date()),
+      bom = TRUE
+    ) 
